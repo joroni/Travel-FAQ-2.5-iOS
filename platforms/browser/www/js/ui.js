776970671cb2c03c138f5bb80f1c7d9c91cefebb
joroni <jraymund.niconi@gmail.com>
2017-08-10 14:37:16 +0800

    ok

--- platforms/browser/www/js/ui.js
+++ platforms/browser/www/js/ui.js
@@ -462,9 +462,10 @@ appUI.populateCountriesAll = function() {
 				
 				var iconPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();
 				
+				var iconPathStatic =  config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();				
 				//var iconPathStatic =  config.localImageFolderPath2 + '/' + code.toLowerCase() + ".png?" + Math.random();				
 				//if (!localFileExists(iconPath) || localFileExists(iconPath)) {
-					
+				console.log(iconPathStatic);
 			if (!localFileExists(iconPath)) {
 					iconPath = config.defaultIconPath;
 					/* SIDE BAR'S ICON */
